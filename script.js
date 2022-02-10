b64f7515f2fedd4d4878e49182f6c8a3356188d0
karankumarjs <karankmr.dev@gmail.com>
2022-02-10 23:26:49 +0530

    increment successful

---
+++ script.js
@@ -0,0 +1,37 @@
+let upperBtn = document.querySelector('#upper-controller')
+let lowerBtn = document.querySelector('#lower-controller')
+let numberList = document.querySelector('.number-list');
+
+// Increment
+lowerBtn.addEventListener('click', function () {
+    toggleNumberActive();
+    addNumber();
+    toggleNumberActive();
+    scrollListUpward()
+})
+
+// Decrement
+upperBtn.addEventListener('click', function () {
+    scrollListdownward()
+    toggleNumberDeactive()
+})
+
+function addNumber() {
+    const newNum = document.createElement('p')
+    newNum.innerText = eval(parseInt(numberList.lastElementChild.innerText) + 1);
+    numberList.appendChild(newNum)
+}
+
+function toggleNumberActive() {
+    let numberListChildrens = numberList.children;
+    let currentActiveNumber = numberListChildrens[eval((numberList.children.length) - 2)];
+    currentActiveNumber.style.transition = "0.5s";
+    currentActiveNumber.classList.toggle('active');
+}
+
+function scrollListUpward() {
+    numberList.scrollBy(0, 128)
+
+}
+
+// Decrement
