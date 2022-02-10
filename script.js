ee379716311b468aac953770c67af5255c2ebd35
karankumarjs <karankmr.dev@gmail.com>
2022-02-11 00:38:05 +0530

    final

--- script.js
+++ script.js
@@ -1,26 +1,57 @@
 let upperBtn = document.querySelector('#upper-controller')
 let lowerBtn = document.querySelector('#lower-controller')
 let numberList = document.querySelector('.number-list');
-
+var audioSound = new Audio('./sound.wav')
 // Increment
+
+// Add the number on click
+
 lowerBtn.addEventListener('click', function () {
     toggleNumberActive();
     addNumber();
     toggleNumberActive();
     scrollListUpward()
+    audioSound.play();
 })
 
+// Add the number on click
+
+window.addEventListener('keyup', function (e) {
+    console.log(e.code);
+    if (e.code == 'ArrowDown') {
+        toggleNumberActive();
+        addNumber();
+        toggleNumberActive();
+        scrollListUpward()
+        audioSound.play();
+
+    }
+})
 
 
 
 // Decrement
+
+// Subtract the number on click
 upperBtn.addEventListener('click', function () {
-    scrollListdownward()
     delNumber();
     toggleNumberDeactive()
     toggleNumberActive()
-
+    audioSound.play();
 })
+// Subtract the number on scroll
+
+window.addEventListener('keyup', function (e) {
+    console.log(e.code);
+    if (e.code == 'ArrowUp') {
+        delNumber();
+        toggleNumberDeactive()
+        toggleNumberActive()
+        audioSound.play();
+
+    }
+})
+
 
 function addNumber() {
     const newNum = document.createElement('p')
@@ -33,6 +64,7 @@ function toggleNumberActive() {
     let currentActiveNumber = numberListChildrens[eval((numberList.children.length) - 2)];
     currentActiveNumber.style.transition = "0.5s";
     currentActiveNumber.classList.toggle('active');
+    console.log(currentActiveNumber);
 }
 
 function scrollListUpward() {
