7e3279921243771ecb685c23c2f380e7daab11f9
karankumarjs <karankmr.dev@gmail.com>
2022-02-10 23:41:35 +0530

    controllers working

--- script.js
+++ script.js
@@ -10,10 +10,16 @@ lowerBtn.addEventListener('click', function () {
     scrollListUpward()
 })
 
+
+
+
 // Decrement
 upperBtn.addEventListener('click', function () {
     scrollListdownward()
+    delNumber();
     toggleNumberDeactive()
+    toggleNumberActive()
+
 })
 
 function addNumber() {
@@ -35,3 +41,17 @@ function scrollListUpward() {
 }
 
 // Decrement
+function delNumber() {
+    numberList.lastElementChild.remove()
+}
+
+function toggleNumberDeactive() {
+    let numberListChildrens = numberList.children;
+    let currentActiveNumber = numberListChildrens[eval((numberList.children.length) - 1)];
+    currentActiveNumber.style.transition = "0.5s";
+    currentActiveNumber.classList.toggle('active');
+}
+
+function scrollListdownward() {
+    numberList.scrollBy(0, -128)
+}
